58207da72c0c1530ceeb501d77c4e59d162c468d
Daiki Maekawa <daaikii@outlook.jp>
2022-11-27 18:43:57 +0900

    チェック状態の都道府県リストをstateで保持する

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
 import { CheckBox } from '@/components/CheckBox'
+import { useCheckPrefs } from '@/hooks/useCheckPrefs'
 import { useGetPrefecturesQuery } from '@/rtk/api'
 
 export default function Home() {
   const { data: prefecturesData, isLoading: isPrefecturesLoading } =
     useGetPrefecturesQuery()
+  const { prefs, checkPrefsHandler } = useCheckPrefs()
+
+  // Todo: 流し込むときにprefsを使う
+  console.log(prefs)
 
   const isLoading = isPrefecturesLoading
   return (
@@ -19,6 +24,7 @@ export default function Home() {
               key={data.prefCode}
               name={data.prefName}
               value={data.prefCode}
+              onChange={checkPrefsHandler}
             />
           ))}
         </>
