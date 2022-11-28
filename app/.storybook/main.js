37c7038391e08ccc31bc5d45d58cbd3abb926802
Daiki Maekawa <daaikii@outlook.jp>
2022-11-29 03:00:08 +0900

    sass-loader導入

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -11,5 +13,33 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1,
+            modules: {
+              localIdentName: "[local]___[hash:base64:2]",
+            },
+          },
+        },
+        "sass-loader",
+      ],
+    });
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@': path.resolve(__dirname, '../src')
+        }
+      }
+    }
   }
 }
---
+++ app/src/stories/components/Button.stories.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/Button'
+import { StoryObj, ComponentMeta } from '@storybook/react'
+
+export default {
+  component: Button
+} as ComponentMeta<typeof Button>
+
+export const Default: StoryObj = {}
