b28745e8d2248e6c85e915fc46a535317255a640
Daiki Maekawa <daaikii@outlook.jp>
2022-11-30 01:43:01 +0900

    ビルドが失敗しないようにする

--- app/next.config.js
+++ app/next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, './src/styles')],
   },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
 module.exports = nextConfig
